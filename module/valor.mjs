05b7085fd1a428b6014636f174a3768107378b5c
GWilliams <gwilliams@csumb.edu>
2023-07-26 16:34:57 -0700

    Added Skill and Flaw effects
    Added Skill and Flaw modifiers that adds  and subtracts from the initial value. Items now correctly allow for effects to affect the actor without needing duplicated effects on actor.

--- module/valor.mjs
+++ module/valor.mjs
@@ -4,6 +4,7 @@ import { valorItem } from "./documents/item.mjs";
 // Import sheet classes.
 import { valorActorSheet } from "./sheets/actor-sheet.mjs";
 import { valorItemSheet } from "./sheets/item-sheet.mjs";
+import { valorActiveEffectConfig } from "./sheets/valor-active-effect-sheet.mjs";
 // Import helper/utility classes and constants.
 import { preloadHandlebarsTemplates } from "./helpers/templates.mjs";
 import { VALOR } from "./helpers/config.mjs";
@@ -24,6 +25,7 @@ Hooks.once('init', async function() {
 
   // Add custom constants for configuration.
   CONFIG.VALOR = VALOR;
+  CONFIG.ActiveEffect.legacyTransferral = false;
 
   /**
    * Set an initiative formula for the system
@@ -37,12 +39,14 @@ Hooks.once('init', async function() {
   // Define custom Document classes
   CONFIG.Actor.documentClass = valorActor;
   CONFIG.Item.documentClass = valorItem;
+  
 
   // Register sheet application classes
   Actors.unregisterSheet("core", ActorSheet);
   Actors.registerSheet("valor", valorActorSheet, { makeDefault: true });
   Items.unregisterSheet("core", ItemSheet);
   Items.registerSheet("valor", valorItemSheet, { makeDefault: true });
+  DocumentSheetConfig.registerSheet(ActiveEffect, 'valor', valorActiveEffectConfig, {makeDefault: true})
 
   // Preload Handlebars templates.
   return preloadHandlebarsTemplates();
